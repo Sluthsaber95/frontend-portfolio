53d0ccad3634a86e541b09864526ce5fc14176b3
Anthony Tran <anthonytran.dev@gmail.com>
2018-07-06 03:39:27 +0100

    Build ES modules exported

---
+++ index.js
@@ -0,0 +1,11 @@
+import NavArrow from './src/NavArrow'
+import NavBar from './src/NavBar'
+import NavColumn from './src/NavColumn'
+import OptionColumn from './src/OptionColumn'
+
+export {
+  NavArrow,
+  NavBar,
+  NavColumn,
+  OptionColumn,
+}
